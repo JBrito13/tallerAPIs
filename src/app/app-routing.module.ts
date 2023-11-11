8c695b4e95bef04cbc4060e0d51c65be14ec83eb
Juan Brito <juan.13.perozo@gmail.com>
2023-11-11 10:39:17 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule)
+  },
+  {
+    path: "devs",
+    loadChildren: () => import("./modules/devs/devs.module").then(m => m.DevsModule),
+    canActivate
+  },
+  {
+    path: "",
+    redirectTo: "auth",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
